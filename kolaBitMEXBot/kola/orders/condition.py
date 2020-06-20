2440e8113697d72c490184f85bc5069fe8fe3a74
mlkbk <mlkbk@local.hp>
2020-06-20 22:16:56 +0000

    debug

--- kolaBitMEXBot/kola/orders/condition.py
+++ kolaBitMEXBot/kola/orders/condition.py
@@ -34,7 +34,7 @@ class Condition:
         """
         self.brg: Bargain = brg
 
-        self.logger = get_logger(logger, sLL="INFO", name=__name__)
+        self.logger = get_logger(logger, sLL="DEBUG", name=__name__)
 
         # Une liste de mots clef pour le prix
         # Fairprice = market price et lastPrice ~= midPrice
